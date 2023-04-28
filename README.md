1db7c2aca954718c7138c7705b9c0e221ae1b26e
ColbyW45 <72152232+ColbyW45@users.noreply.github.com>
2023-04-28 08:07:14 -0400

    CSV Load Working

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ASL-Speed-Practice
+# ASL Speed Practice
+
 An application to help in learning and practicing signing at speed
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os, sys, pandas
 
 from PyQt6.QtWidgets import (
 	QApplication, QMainWindow, QWidget, QPushButton, QSlider, QLabel, QMenuBar, QMenu, QToolBar, QSpacerItem, QCheckBox,
-	QProgressBar, QLineEdit, QVBoxLayout, QHBoxLayout, QGridLayout, QMdiArea, QGraphicsScene, QGraphicsView
+	QProgressBar, QLineEdit, QVBoxLayout, QHBoxLayout, QGridLayout, QMdiArea, QGraphicsScene, QGraphicsView, QFileDialog
 )
 from PyQt6 import QtGui, QtCore
-from PyQt6.QtCore import (Qt, QObject, QUrl, QThread, pyqtSignal)
+from PyQt6.QtCore import (Qt, QObject, QUrl, QThread)
 from PyQt6.QtGui import (QIcon, QFont, QGuiApplication, QColor)
 
 class Window(QMainWindow):
@@ -21,11 +21,6 @@ class Window(QMainWindow):
 		self.setContentsMargins(5, 5, 5, 5)
 		self.resize(750,500)
 
-		# Load data (move later)
-		lettersDict = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 
-		   			15:"O", 16:"P", 17:"Q", 18:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"}
-		self.R = Randomizer(lettersDict)
-		
 		# Menu Bar
 		appMenu = QMenuBar()
 		fileMenuItem = QMenu("&File", self)
@@ -77,11 +72,11 @@ class Window(QMainWindow):
 		self.delayInput.valueChanged.connect(self.DelayUpdated)
 
 		# Defines timer ON RUNTIME, updated in self.DelayUpdated()
-		# Interval in milliseconds
-		self.timer = QtCore.QTimer(self, interval=self.delayInput.value() * 5, timeout=self.Run)
+		# Interval in milliseconds, 1000ms = 1s
+		self.timer = QtCore.QTimer(self, interval=self.delayInput.value()*5, timeout=self.Run)
 
 		# Add delay input field value indicator
-		self.delayInputVal = QLabel(str(self.delayInput.value()*0.25))
+		self.delayInputVal = QLabel(str(self.delayInput.value()*0.25+1))
 		self.delayInputVal.setMaximumWidth(25)
 		settingsToolbarLayout.addWidget(self.delayInputVal)
 
@@ -118,8 +113,37 @@ class Window(QMainWindow):
 
 		return settingsToolbarWidget
 
-		
 	def Home(self):
+		central_widget = QWidget()
+		central_widget_layout = QHBoxLayout()
+		central_widget.setLayout(central_widget_layout)
+		self.setCentralWidget(central_widget)
+
+		self.textPracticeMode = QPushButton("CSV")
+		central_widget_layout.addWidget(self.textPracticeMode)
+
+		self.textPracticeMode.clicked.connect(self.CSV_file_select)
+		self.textPracticeMode.clicked.connect(self.text_practice_mode)
+
+	def CSV_file_select(self):
+		fileDialogReturn = QFileDialog.getOpenFileName(self, 'Open file', 
+         '.\data',"CSV files (*.csv)")
+		# Returns a tuple object
+		fileName = fileDialogReturn[0]
+    	
+		#print(type(fileDialogReturn), fileDialogReturn)
+		#print(fileName)
+
+		self.practceSetDict = Data.CSV_Handler(fileName)
+		self.R = Randomizer(self.practceSetDict)
+	
+	def text_practice_mode(self):
+
+		self.Practice_UI()
+
+
+
+	def Practice_UI(self):
 		#toolbar = self.SettingsToolBar()
 		#self.addToolBar(toolbar)
 
@@ -132,7 +156,7 @@ class Window(QMainWindow):
 		left_column_layout = QGridLayout()
 		self.left_column_widget.setLayout(left_column_layout)
 		
-		self.leftColumnMinWidth = 35
+		self.leftColumnMinWidth = 0
 		self.leftColumnMaxWidth = 100
 
 		central_widget_layout.addWidget(self.left_column_widget)
@@ -146,11 +170,11 @@ class Window(QMainWindow):
 		#left_column_layout.addWidget(QWidget(), 0, 0, Qt.AlignmentFlag.AlignTop)
 
 		# Load file menu toggle
-		self.fileMenuToggleState = False
+		'''self.fileMenuToggleState = False
 		self.fileMenuToggle = QPushButton("T")
 		self.fileMenuToggle.setMaximumWidth(15)
 		self.fileMenuToggle.clicked.connect(self.File_Menu_View_Toggle)
-		left_column_layout.addWidget(self.fileMenuToggle, 1, 0, Qt.AlignmentFlag.AlignTop)
+		left_column_layout.addWidget(self.fileMenuToggle, 1, 0, Qt.AlignmentFlag.AlignTop)'''
 
 		#self.load_file_button = QPushButton("Load File")
 		#left_column_layout.addWidget(self.load_file_button)
@@ -179,11 +203,12 @@ class Window(QMainWindow):
 		
 		# Progress Bar
 		self.progBarVal = 0
-		self.progBarMax = 250
+		self.progBarMax = 100
 
 		self.progressBar = QProgressBar()
 		self.progressBar.setFixedHeight(20)
 		self.progressBar.setMaximum(self.progBarMax)
+		self.progressBar.setFormat("")
 		right_column_layout.addWidget(self.progressBar) 
 		
 		self.setCentralWidget(central_widget)
@@ -192,6 +217,7 @@ class Window(QMainWindow):
 		self.pauseButton.clicked.connect(self.pauseButtonPressed)
 
 	def FilePreviewWindow(self):
+		# UNUSED
 		file_list = QWidget()
 		fl_layout = QGridLayout()
 		file_list.setLayout(fl_layout)
@@ -219,10 +245,6 @@ class Window(QMainWindow):
 	def pauseButtonPressed(self):
 		self.timer.stop()
 
-	def Home_To_File_Preview_Window(self):
-		#self.setcentralWidget()
-		pass
-
 	def File_Menu_View_Toggle(self):
 		if self.fileMenuToggleState == False:
 			self.fileMenuToggleState = True
@@ -231,9 +253,6 @@ class Window(QMainWindow):
 		elif self.fileMenuToggleState == True:
 			self.fileMenuToggleState = False
 			self.left_column_widget.setFixedWidth(self.leftColumnMinWidth)
-
-		
-
 
 
 	def Run(self):
@@ -248,9 +267,9 @@ class Window(QMainWindow):
 			self.UpdateGDText()
 			
 	def DelayUpdated(self):
-		self.delayInputVal.setText(str(self.delayInput.value()*0.25))
+		self.delayInputVal.setText(str(self.delayInput.value()*0.25+1))
 		# Interval in milliseconds
-		self.timer.setInterval(self.delayInput.value() * 5)
+		self.timer.setInterval(self.delayInput.value()*5)
 		
 	def TextGraphicDisplay(self):
 		self.scene = QGraphicsScene()
@@ -263,13 +282,16 @@ class Window(QMainWindow):
 		self.textObject = self.scene.addSimpleText(text, displayFont)
 
 		self.GraphicsView = QGraphicsView(self.scene, self)
-		#self.GraphicsView.
-		#GraphicsView.setGeometry(0,0,600,500)
+		
+		#self.GraphicsView.centerOn(100.0, 100.0)
+		#self.GraphicsView.setGeometry(0,0,600,500)
 
 	def UpdateGDText(self, displayString="Error: could not load display element"):
 		displayString = self.R.randomStr()
 		#print(displayString)
 		self.textObject.setText(displayString)
+		#self.GraphicsView.centerOn(self.textObject)
+		#self.GraphicsView.centerOn(0.0, 0.0)
 
 
 class Randomizer():
@@ -279,23 +301,30 @@ class Randomizer():
 		self._testStr = "Test string"
 
 	def randomStr(self):			
-		itemsKey = r.randint(1,26)
+		itemsKey = r.randint(1,len(self.items)-1)
+		# Ignores Header by starting at 1
 		a = self.items[itemsKey]
-			
+		
 		return a
 	
 class Data():
-	lettersDict = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 
-		   			15:"O", 16:"P", 17:"Q", 18:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"}
 	
-	def CSV_Handler():
-		file = r'.\data\alphabet.csv'
+	def CSV_Handler(file) -> dict:
+		#file = r'.\data\alphabet.csv'
+		# Test file
+		output = {}
 
 		csv_ds = pandas.read_csv(file, header=0)
 		print(csv_ds)
 		
-		test = dict(csv_ds)
-		print(test)
+		for row in range(len(csv_ds)):
+			row_contents = csv_ds.loc[row]
+			#print(row_contents[0])
+
+			output[row] = row_contents[0]
+
+		print(output)
+		return output
 
 
 
@@ -306,8 +335,6 @@ def main() -> None :
 	
 	window = Window()
 	window.show()
-
-	Data.CSV_Handler()
 
 	sys.exit(app.exec())
 
